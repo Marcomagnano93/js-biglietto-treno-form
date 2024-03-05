291e27c8552107806a8e7f6ce94c3f1e02371d26
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-05 14:08:05 +0100

    FIRST COMMIT

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//prendere i km inseriti dall'utente nel form
+
+
+let km
+
+    km = document.getElementById("distance")
+
+let inputKm = km.value
+
+    console.log(inputKm)
+
+
+// prendere l'età inserita dall'utente nel form
+let age
+
+    age = document.getElementById("age")
+
+let inputAge = age.value
+
+    console.log(inputAge)
+
+
